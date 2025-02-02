6385aab80e35bc3372ce3d9f12e5ae2cd5c8be7c
Lindsey Lasswell <81826963+kclibrarian@users.noreply.github.com>
2025-02-01 19:48:41 -0600

    Fix home button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,7 +40,7 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
+        <Navbar.Brand href="/Portfolio" className="d-flex">
           <SiCoderwall alt="brand" />
         </Navbar.Brand>
         <Navbar.Toggle
@@ -56,7 +56,11 @@ function NavBar() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+              <Nav.Link
+                as={Link}
+                to="/Portfolio"
+                onClick={() => updateExpanded(false)}
+              >
                 <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
               </Nav.Link>
             </Nav.Item>
